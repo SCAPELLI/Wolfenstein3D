a82145e10be075989048fd59fa5c46013c6d662b
xNicky <nraveszani@gmail.com>
2020-12-12 13:48:05 -0300

    Ahora, hay un boton en EditorScene para salvar. Al tocar el boton, si es la primera vez que se guarda el mapa nuevo se abre una ventana para elegir donde guardar el archivo. De momento, solo se crea un archivo. Todavia no se modifica el contenido del archivo

--- Editor/include/EditorScene.h
+++ Editor/include/EditorScene.h
@@ -20,12 +20,14 @@ class EditorScene : public QMainWindow {
         ScreenManager *screenManager;
         TextureList *textureList;
         Tilemap *tilemap;
-        std::ofstream mapFilePath;
+        std::ofstream mapFile;
 
     public:
         EditorScene(QWidget *parent = 0, ScreenManager *screenManager = NULL);
         ~EditorScene();
         void setMapSize(size_t rows, size_t columns);
+        void connectEvents();
+        void saveMap();
 
 };
 
--- Editor/src/EditorScene.cpp
+++ Editor/src/EditorScene.cpp
@@ -1,3 +1,7 @@
+#include <QtWidgets/QPushButton>
+#include <iostream>
+#include <QtWidgets/QFileDialog>
+#include <QDir>
 #include "../include/EditorScene.h"
 #include "ui_EditorScene.h"
 
@@ -13,13 +17,84 @@ EditorScene::EditorScene(QWidget *parent, ScreenManager *screenManager)
     this->tilemap = new Tilemap();
     QVBoxLayout *tilemapLayout = findChild<QVBoxLayout*>("tilemapLayout");
     tilemapLayout->addWidget(this->tilemap);
+
+    connectEvents();
 }
 
 EditorScene::~EditorScene() {
     delete this->ui;
     delete this->textureList;
+    delete this->tilemap;
 }
 
 void EditorScene::setMapSize(size_t rows, size_t columns) {
     this->tilemap->setMapSize(rows, columns);
 }
+
+void EditorScene::connectEvents() {
+    QPushButton* button = findChild<QPushButton*>("saveButton");
+    QObject::connect(button, &QPushButton::clicked, this, &EditorScene::saveMap);
+}
+
+void EditorScene::saveMap() {
+    //Si ya existe, guardo ahi
+    if (this->mapFile.is_open()) {
+        std::cout << "se guardo el archivo\n";
+        // GUARDAR EL ARCHIVO
+        // LEVANTAR UNA VENTANA QUE INFORME QUE SE GUARDO
+    } else {
+        QString filter = "Yaml File (*.yaml)";
+        QString filePath = QFileDialog::getSaveFileName(this,
+                                                        "Save the file",
+                                                        QDir::homePath(),
+                                                        filter);
+        std::string aux = filePath.toUtf8().toStdString() + ".yaml";
+        std::cout << aux << '\n';
+        this->mapFile.open(aux, std::ios::out);
+        if (!this->mapFile.is_open()) {
+            std::cout << "no se pudo abrir el archivo\n";
+            // LEVANTAR UNA VENTANITA QUE LO INFORME
+        } else {
+            // GUARDAR EL ARCHIVO
+            // LRVANTAR UNA VENTANA QUE LO INFORME
+        }
+    }
+}
+
+
+
+/*
+
+QString filter = "Yaml File (*.yaml)";
+//QString file = QFileDialog::getOpenFileName(this, "Open a file", QDir::homePath(), filter);
+QFileDialog qFileDialog;
+QString filePath = qFileDialog.getSaveFileName(this,
+                                        "Save file",
+                                        QDir::homePath(),
+                                        filter);
+std::cout << filePath.toUtf8().constData();
+QFile file(filePath);
+if (!file.open(QIODevice::ReadOnly | QIODevice::WriteOnly | QIODevice::Text)) {
+std::cout << "no se abrio";
+}
+
+if (this->mapFile.is_open()) {
+    std::cout << "se guardo el archivo\n";
+    // GUARDAR EL ARCHIVO
+} else {
+    QString filter = "Yaml File (*.yaml)";
+    QString filePath = QFileDialog::getOpenFileName(this,
+                                                "Open a file",
+                                                QDir::homePath(),
+                                                filter);
+    std::string aux = filePath.toUtf8().toStdString() + ".yaml";
+    std::cout << aux << '\n';
+    this->mapFile.open(aux, std::ios::out);
+    if (!this->mapFile.is_open()) {
+        std::cout << "no se pudo abrir el archivo\n";
+        // LEVANTAR UNA VENTANITA QUE LO INFORME
+    } else {
+        // GUARDAR EL ARCHIVO
+    }
+}
+*/
