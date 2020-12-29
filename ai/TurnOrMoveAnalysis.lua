9db00e20c6c4e4e91ea509117ded23ea1aed12d0
Sebastian Capelli <scapelli@fi.uba.ar>
2020-12-28 23:59:35 -0300

    ripper mode implementado, el bot seguirà al primer player detectado hasta que logre matarlo

---
+++ ai/TurnOrMoveAnalysis.lua
@@ -0,0 +1,105 @@
+PI = 3.14159265358979323846
+
+MOVE_FOWARD = "FOWARD"
+TURN_ANTICLOCKWISE =  "ANTICLOCKWISE"
+TURN_CLOCKWISE = "CLOCKWISE"
+
+function valueIsInRange(value, lowerBound, upperBound)
+    return lowerBound <= value and value < upperBound
+end
+
+function botAngleIsInTolerableRange(botAngle, idealAngle)
+    tolerance = PI/10
+    if idealAngle + tolerance >= 2 * PI then
+        return 
+            valueIsInRange(botAngle, 0, idealAngle+tolerance-2*PI) 
+            or
+            valueIsInRange(botAngle, idealAngle-tolerance, 2*PI)
+    else 
+        if idealAngle - tolerance < 0 then
+        return 
+            valueIsInRange(botAngle, 0, idealAngle+tolerance)
+            or
+            valueIsInRange(botAngle, idealAngle-tolerance+2*PI, 2*PI)
+        else
+            return valueIsInRange(botAngle, idealAngle-tolerance, idealAngle+tolerance)
+        end
+    end
+end
+
+function turnAnalyzer(botAngle, idealAngle)
+    local alfa = botAngle - idealAngle
+    local beta = alfa + 2*PI
+    local gamma = alfa - 2*PI
+    
+    local minimum = alfa
+    if math.abs(minimum) > math.abs(beta) then
+      minimum = beta
+    end
+    if math.abs(minimum) > math.abs(gamma) then
+      minimum = gamma
+    end
+    
+    if minimum > 0 then 
+    	return TURN_CLOCKWISE
+    else
+    	return TURN_ANTICLOCKWISE
+    end
+end
+
+function getIdealAngle(botPosition, idealPosition)
+  local idealAngle = math.atan ( (idealPosition["y"] - botPosition["y"])
+	                             /
+	                             (idealPosition["x"] - botPosition["x"]))
+  local adjusment = 0
+  
+  if 
+    (botPosition["x"] > idealPosition["x"] and botPosition["y"] <= idealPosition["y"]) 
+    or
+    (botPosition["x"] > idealPosition["x"] and botPosition["y"] > idealPosition["y"])
+  then
+      adjusment = PI
+  else 
+    if (botPosition["x"] <= idealPosition["x"] and botPosition["y"] > idealPosition["y"]) then
+      adjusment = 2*PI
+    end
+  end
+  
+  return idealAngle + adjusment
+end
+
+function turnOrMoveAnalyzer(bot, idealXPosition, idealYPosition)
+    local idealPosition = {}
+    idealPosition["x"] = idealXPosition
+    idealPosition["y"] = idealYPosition
+	
+    local botPosition = bot["position"] --tener harcodeado el (0,0)
+	local botAngle = bot["angle"] --En angulo debe estar "normarlizado" entre 0 y 2PI
+
+	local idealAngle = getIdealAngle(botPosition, idealPosition)
+
+	if botAngleIsInTolerableRange(botAngle, idealAngle) then
+	    return MOVE_FOWARD
+    else
+    	return turnAnalyzer(botAngle, idealAngle)
+	end
+end
+
+--ejemplo de uso
+--[[
+players = { 
+    ["1"] = {
+        ["position"] = { ["x"] = 3, ["y"] = 4 },
+        ["angle"] = 0
+    },
+    ["bot"] = {
+
+        ["position"] = { ["x"] = 16 , ["y"] = 48 },
+        ["angle"] = 0
+    }
+}
+
+
+print(turnOrMoveAnalyzer(players["bot"], 14, 14))
+
+]]
